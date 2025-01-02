327eb9c96d9ea7991e7e11c615446319da5f5e83
iamsaura8h <saurabhchaudhary.net@gmail.com>
2025-01-02 11:05:39 +0530

    fixed minor issues

--- src/pages/Calculation.tsx
+++ src/pages/Calculation.tsx
@@ -3,11 +3,11 @@ import ShortAC from "../components/ShortAC";
 function Calculation() {
   return (
     <>
-      <div className="min-w-screen min-h-screen flex flex-col sm:flex-row-reverse justify-normal p-5" id="calculator_view">
-        <div>
+      <div className="min-w-screen h-min flex flex-col sm:flex-row-reverse justify-center p-5" id="calculator_view">
+        
           <ShortAC />
-        </div>
-        <img src="img2.svg" alt="" className="w-full md:w-1/2" />
+     
+        <img src="img2.svg" alt="" className="w-full h-min md:w-1/2 mt-12" />
       </div>
     </>
   );
